b495b9225f7cb2e96953c5a9d4b4fa820012ef63
Eduardo Sánchez <jeduardo.schz@gmail.com>
2022-02-06 22:42:10 -0600

    Create development.js

---
+++ config/development.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  database: {
+    connection: {
+      host: '127.0.0.1', 
+      database: 'bank',
+      user: 'postgres',
+      password: 'postgres',
+    },
+  },
+  fixer: {
+    accessKey: 'fe32622712536fdef8e50d40a014937c',
+  },
+}
